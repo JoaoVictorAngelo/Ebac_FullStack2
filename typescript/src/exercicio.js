35065ced36b9e761c31f88814532c2277efebfb6
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2025-02-24 12:55:40 -0300

    Exercicio de TypeScript

---
+++ typescript/src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicar(6, 7));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao('João'));
---
+++ typescript/src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar (num1: number, num2: number ): number{
+    return num1 * num2;
+}
+console.log(multiplicar(6, 7))
+
+function saudacao (nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(saudacao('João'))
